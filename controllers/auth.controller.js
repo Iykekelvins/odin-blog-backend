a725d295180b02a5f8f1ef8821c3ceb1da734380
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-04-15 00:05:24 +0100

    feat: add login controller with jtwt issuing

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,14 +3,13 @@ import { validateSignup } from '../lib/validators.js';
 import { matchedData, validationResult } from 'express-validator';
 
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const signupController = [
 	validateSignup,
 	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
-			console.log(errors);
-
 			return res.status(400).json({
 				error: errors.array()[0].msg,
 			});
@@ -41,6 +40,57 @@ const signupController = [
 	},
 ];
 
-const loginController = (req, res) => {};
+const loginController = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		if (!email) {
+			return res.status(400).json({
+				error: 'Email is required',
+			});
+		}
+
+		if (!password) {
+			return res.status(400).json({
+				error: 'Password is required',
+			});
+		}
+
+		const user = await prisma.user.findUnique({
+			where: { email },
+		});
+
+		if (!user) {
+			return res.status(404).json({
+				error: 'Invalid credentials',
+			});
+		}
+
+		const match = await bcrypt.compare(password, user.password);
+
+		if (!match) {
+			return res.status(400).json({
+				error: 'Invalid credentials',
+			});
+		}
+
+		const token = jwt.sign(
+			{ id: user.id, role: user.role },
+			process.env.JWT_SECRET,
+			{ expiresIn: '1d' },
+		);
+
+		const { password: userPw, ...rest } = user;
+
+		res.status(200).json({
+			message: 'Login successful',
+			user: { ...rest, token },
+		});
+	} catch (error) {
+		res.status(500).json({
+			error: 'Something went wrong. Try again later.',
+		});
+	}
+};
 
 export { signupController, loginController };
