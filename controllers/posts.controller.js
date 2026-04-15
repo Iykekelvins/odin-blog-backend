6370393b24888990d35e095a7c93ac12f52fe3a5
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-04-15 02:38:50 +0100

    feat: add routes and controllers for getting editting and deleting a single post

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -78,4 +78,113 @@ const getPosts = [
 	},
 ];
 
-export { createPost, getPosts };
+const getSinglepost = [
+	authMiddleware,
+	async (req, res) => {
+		try {
+			const postId = req.params.postId;
+
+			const post = await prisma.post.findUnique({
+				where: { id: postId, authorId: req.user.id },
+				omit: {
+					authorId: true,
+				},
+			});
+
+			if (!post) {
+				return res.status(404).json({ error: 'Post not found' });
+			}
+
+			res.status(200).json({
+				message: 'Post fetched successfully',
+				post,
+			});
+		} catch (error) {
+			res.status(500).json({
+				error: error.message || 'Something went wrong. Try again later.',
+			});
+		}
+	},
+];
+
+const editPost = [
+	authMiddleware,
+	async (req, res) => {
+		const { title, content } = req.body;
+
+		try {
+			const postId = req.params.postId;
+
+			const post = await prisma.post.findUnique({
+				where: { id: postId, authorId: req.user.id },
+			});
+
+			if (!post) {
+				return res.status(404).json({ error: 'Post not found' });
+			}
+
+			// check if post belongs to author
+			if (post.authorId !== req.user.id) {
+				return res
+					.status(403)
+					.json({ error: 'Forbidden - You cannot edit this post' });
+			}
+
+			const updatedPost = await prisma.post.update({
+				where: { id: postId },
+				data: {
+					title,
+					content,
+				},
+				omit: {
+					authorId: true,
+				},
+			});
+
+			res.status(200).json({
+				message: 'Post updated successfully',
+				post: updatedPost,
+			});
+		} catch (error) {
+			res.status(500).json({
+				error: error.message || 'Something went wrong. Try again later.',
+			});
+		}
+	},
+];
+
+const deletePost = [
+	authMiddleware,
+	async (req, res) => {
+		try {
+			const postId = req.params.postId;
+
+			const post = await prisma.post.findUnique({
+				where: { id: postId, authorId: req.user.id },
+			});
+
+			if (!post) {
+				return res.status(404).json({ error: 'Post not found' });
+			}
+
+			// check if post belongs to author
+			if (post.authorId !== req.user.id) {
+				return res
+					.status(403)
+					.json({ error: 'Forbidden - You cannot delete this post' });
+			}
+
+			await prisma.post.delete({
+				where: { id: postId },
+			});
+
+			res.sendStatus(204);
+		} catch (error) {
+			res.status(500).json({
+				error: error.message || 'Something went wrong. Try again later.',
+			});
+		}
+	},
+];
+
+export { createPost, getPosts, getSinglepost, editPost, deletePost };
--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-import { createPost, getPosts } from '../controllers/posts.controller.js';
+import {
+	createPost,
+	deletePost,
+	editPost,
+	getPosts,
+	getSinglepost,
+} from '../controllers/posts.controller.js';
 
 const router = Router();
 
 router.post('/', createPost);
 router.get('/', getPosts);
+router.get('/:postId', getSinglepost);
+router.patch('/:postId', editPost);
+router.delete('/:postId', deletePost);
 
 export default router;
